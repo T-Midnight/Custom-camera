1d2dce212f260e2b16ad40ee88e5d900c6a7f769
Tatiana Belousova <zirael36@gmail.com>
2019-12-13 13:32:35 +0400

    Add spinner, files for pictures

--- app/src/main/java/com/example/customcamera/MainActivity.kt
+++ app/src/main/java/com/example/customcamera/MainActivity.kt
@@ -19,11 +19,17 @@ import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Surface
 import android.view.TextureView
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity()  {
@@ -39,8 +45,8 @@ class MainActivity : AppCompatActivity()  {
     private var cameraId = ""
     private var camera1 = 0
     private var camera2 = 1
-
-//TODO Обработка варианта поворота экрана, чтоб не крашился
+    var formattedDate = ""
+    var countRepeat = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,8 +56,7 @@ class MainActivity : AppCompatActivity()  {
             requestPermissions(
                 arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
         } else {
-            mCameraManager =
-                appContext?.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+            mCameraManager = appContext?.getSystemService(Context.CAMERA_SERVICE) as CameraManager
             cameraId = getCameraWithFacing(mCameraManager!!, LENS_FACING_BACK)
             for (camera in mCameraManager!!.cameraIdList) {
                 myCameras.add(CameraService(mCameraManager!!, camera, this))
@@ -59,7 +64,11 @@ class MainActivity : AppCompatActivity()  {
             surfaceTextureListener = object : TextureView.SurfaceTextureListener {
                 override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture?, p1: Int, p2: Int) { }
 
-                override fun onSurfaceTextureUpdated(p0: SurfaceTexture?) { }
+                override fun onSurfaceTextureUpdated(p0: SurfaceTexture?) {
+                    if (myCameras[camera1] != null)
+                        if (!myCameras[camera1].isOpen)
+                            myCameras[camera1].openCamera()
+                }
 
                 @SuppressLint("NewApi")
                 override fun onSurfaceTextureDestroyed(p0: SurfaceTexture?): Boolean {
@@ -67,12 +76,34 @@ class MainActivity : AppCompatActivity()  {
                 }
 
                 override fun onSurfaceTextureAvailable(p0: SurfaceTexture?, p1: Int, p2: Int) {
-                    if (myCameras[camera1] != null)
-                        if (!myCameras[camera1].isOpen)
-                            myCameras[camera1].openCamera()
+                        if (myCameras[camera1] != null)
+                            if (!myCameras[camera1].isOpen)
+                                myCameras[camera1].openCamera()
                 }
             }
             texture_view.surfaceTextureListener = surfaceTextureListener
+
+
+            val date = Calendar.getInstance().time
+            val formatter = SimpleDateFormat("yyyyMMdd_HHmmss")
+            formattedDate = formatter.format(date)
+
+            ArrayAdapter.createFromResource(this, R.array.counts_array, R.layout.custom_spinner_item).also {
+                adapter ->
+                    adapter.setDropDownViewResource(R.layout.custom_spinner_dropdown)
+                    spinner.adapter = adapter
+            }
+
+            spinner.apply {
+                setSelection(0)
+                onItemSelectedListener = object : OnItemSelectedListener {
+                    override fun onNothingSelected(p0: AdapterView<*>?) { }
+
+                    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+                        countRepeat = position + 1
+                    }
+                }
+            }
         }
     }
 
@@ -131,10 +162,8 @@ class MainActivity : AppCompatActivity()  {
         }
         for (cameraId in cameraIdList) {
             val characteristics = manager.getCameraCharacteristics(cameraId)
-            // Получения списка выходного формата, который поддерживает камера
             val map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
                 ?: continue
-            //  Определение какая камера куда смотрит
             val facing = characteristics.get(CameraCharacteristics.LENS_FACING)
             if (facing != null && facing == lensFacing) {
                 myCameraId = cameraId
@@ -146,11 +175,9 @@ class MainActivity : AppCompatActivity()  {
                 for (item in sizesJPEG) {
                     Log.d("Camera", "w:" + item.width + " h:" + item.height)
                 }
-//                w:2592 h:1936
             }
 
-            possibleCandidate =
-                myCameraId  //just in case device don't have any camera with given facing
+            possibleCandidate = myCameraId
         }
         return possibleCandidate ?: cameraIdList[0]
     }
@@ -208,16 +235,27 @@ class MainActivity : AppCompatActivity()  {
         private lateinit var mCaptureSession: CameraCaptureSession
         private lateinit var mCameraCallback: CameraDevice.StateCallback
         var mImageReader: ImageReader? = null
-        private var file: File = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "your best photo.jpg")
+        var repeats = 0
+//        private var file: File = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "your best photo.jpg")
+
+        var files: ArrayList<File> = createFiles()
 
         private val mOnImageAvailableListener: ImageReader.OnImageAvailableListener =
             ImageReader.OnImageAvailableListener { reader ->
-                mBackgroundHandler!!.post(ImageSaver(reader.acquireNextImage(), file))
+                mBackgroundHandler!!.post(ImageSaver(reader.acquireNextImage(), files[repeats]))
+                repeats++
             }
 
 
         val isOpen: Boolean
             get() = mCameraDevice != null
+
+        private fun createFiles(): ArrayList<File> {
+            for (i in 1..countRepeat) {
+                files.add(File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "IMG_CustomCamera_${formattedDate}.jpg"))
+            }
+            return files
+        }
 
         fun openCamera() {
             try {
@@ -229,23 +267,15 @@ class MainActivity : AppCompatActivity()  {
                     mCameraCallback = object : CameraDevice.StateCallback() {
                         override fun onOpened(cameraDevice: CameraDevice) {
                             mCameraDevice = cameraDevice
-                            Log.d("Camera", "Open camera  with id:" + mCameraDevice!!.id)
                             createCameraPreviewSession(activity)
                         }
 
                         override fun onDisconnected(p0: CameraDevice) {
                             mCameraDevice!!.close()
-                            Log.d("Camera", "disconnect camera  with id:" + mCameraDevice!!.id)
-//                        mCameraDevice = null
+                            mCameraDevice = null
                         }
 
-                        override fun onError(cameraDevice: CameraDevice, error: Int) {
-                            Log.d(
-                                "Camera",
-                                "Error!!! camera id:" + cameraDevice.id + " error:" + error
-                            )
-                        }
-
+                        override fun onError(cameraDevice: CameraDevice, error: Int) { }
                     }
                     mCameraManager.openCamera(mCameraID, mCameraCallback, mBackgroundHandler)
                 }
@@ -257,7 +287,6 @@ class MainActivity : AppCompatActivity()  {
             mImageReader = ImageReader.newInstance(2592, 1936, ImageFormat.JPEG, 1)
             mImageReader!!.setOnImageAvailableListener(mOnImageAvailableListener, null)
 
-//TODO
             var textureView = activity.findViewById<TextureView>(R.id.texture_view)
             var texture: SurfaceTexture = textureView.surfaceTexture
             texture.setDefaultBufferSize(2592, 1936)
@@ -288,11 +317,9 @@ class MainActivity : AppCompatActivity()  {
             }
         }
 
-
         fun closeCamera() {
             mCameraDevice!!.close()
-//            mCameraDevice = null
-
+            mCameraDevice = null
         }
 
         fun makePhoto() {
@@ -341,197 +368,3 @@ class MainActivity : AppCompatActivity()  {
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-////        texture_view.surfaceTextureListener = object: TextureView.SurfaceTextureListener {
-////            override fun onSurfaceTextureDestroyed(p0: SurfaceTexture?): Boolean {
-////                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-////            }
-////
-////            override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture?, p1: Int, p2: Int) { }
-////
-////            override fun onSurfaceTextureUpdated(p0: SurfaceTexture?) { }
-////
-////            override fun onSurfaceTextureAvailable(surface: SurfaceTexture?, width: Int, height: Int) {
-////                surface?.let {
-////                    mOnSurfaceTextureAvailable.onNext(surface)
-////                }
-////            }
-////        }
-//    }
-//    fun openCamera(
-//        cameraId: String,
-//        cameraManager: CameraManager
-//    ): Observable<Pair<DeviceStateEvents, CameraDevice>> {
-//        return Observable.create { observableEmitter ->
-//            cameraManager.openCamera(cameraId, object : CameraDevice.StateCallback() {
-//                override fun onOpened(cameraDevice: CameraDevice) {
-//                    observableEmitter.onNext(Pair(DeviceStateEvents.ON_OPENED, cameraDevice))
-//                }
-//
-//                override fun onClosed(cameraDevice: CameraDevice) {
-//                    observableEmitter.onNext(Pair(DeviceStateEvents.ON_CLOSED, cameraDevice))
-//                    observableEmitter.onComplete()
-//                }
-//
-//                override fun onDisconnected(cameraDevice: CameraDevice) {
-//                    observableEmitter.onNext(Pair(DeviceStateEvents.ON_DISCONNECTED, cameraDevice))
-//                    observableEmitter.onComplete()
-//                }
-//
-//                override fun onError(camera: CameraDevice, error: Int) {
-//                    observableEmitter.onError(
-//                        OpenCameraException(OpenCameraException.Reason.getReason(error))
-//                    )
-//                }
-//            }, null)
-//        }
-//    }
-//
-//    fun createCaptureSession(
-//        cameraDevice: CameraDevice, surfaceList: List<Surface>
-//    ): Observable<Pair<CaptureSessionStateEvents, CameraCaptureSession>> {
-//        return Observable.create { observableEmitter ->
-//            cameraDevice.createCaptureSession(
-//                surfaceList,
-//                object : CameraCaptureSession.StateCallback() {
-//
-//                    override fun onConfigured(session: CameraCaptureSession) {
-//                        observableEmitter.onNext(Pair(CaptureSessionStateEvents.ON_CONFIGURED, session))
-//                    }
-//
-//                    override fun onConfigureFailed(session: CameraCaptureSession) {
-//                        observableEmitter.onError(CreateCaptureSessionException(session))
-//                    }
-//
-//                    override fun onReady(session: CameraCaptureSession) {
-//                        observableEmitter.onNext(Pair(CaptureSessionStateEvents.ON_READY, session))
-//                    }
-//
-//                    override fun onActive(session: CameraCaptureSession) {
-//                        observableEmitter.onNext(Pair(CaptureSessionStateEvents.ON_ACTIVE, session))
-//                    }
-//
-//                    override fun onClosed(session: CameraCaptureSession) {
-//                        observableEmitter.onNext(Pair(CaptureSessionStateEvents.ON_CLOSED, session))
-//                        observableEmitter.onComplete()
-//                    }
-//
-//                    override fun onSurfacePrepared(session: CameraCaptureSession, surface: Surface) {
-//                        observableEmitter.onNext(Pair(CaptureSessionStateEvents.ON_SURFACE_PREPARED, session))
-//                    }
-//                },
-//                null
-//            )
-//        }
-//    }
-//
-//    private fun createCaptureCallback(observableEmitter: ObservableEmitter<CaptureSessionData>): CameraCaptureSession.CaptureCallback {
-//        return object : CameraCaptureSession.CaptureCallback() {
-//
-//            override fun onCaptureStarted(session: CameraCaptureSession, request: CaptureRequest, timestamp: Long, frameNumber: Long) {}
-//
-//            override fun onCaptureProgressed(session: CameraCaptureSession, request: CaptureRequest, partialResult: CaptureResult) { }
-//
-//            override fun onCaptureCompleted(session: CameraCaptureSession, request: CaptureRequest, result: TotalCaptureResult) {
-//                if (!observableEmitter.isDisposed) {
-//                    observableEmitter.onNext(CaptureSessionData(CaptureSessionEvents.ON_COMPLETED, session, request, result))
-//                }
-//            }
-//
-//            override fun onCaptureFailed(session: CameraCaptureSession, request: CaptureRequest, failure: CaptureFailure) {
-//                if (!observableEmitter.isDisposed) {
-//                    observableEmitter.onError(CameraCaptureFailedException(failure))
-//                }
-//            }
-//
-//            override fun onCaptureSequenceCompleted(session: CameraCaptureSession, sequenceId: Int, frameNumber: Long) { }
-//
-//            override fun onCaptureSequenceAborted(session: CameraCaptureSession, sequenceId: Int) {}
-//        }
-//    }
-//
-//    fun fromSetRepeatingRequest(captureSession: CameraCaptureSession , request: CaptureRequest): Observable<CaptureSessionData> {
-//        return Observable
-//            .create { observableEmitter -> captureSession .setRepeatingRequest(request, createCaptureCallback(observableEmitter), null)}
-//    }
-//
-//    fun fromCapture(captureSession: CameraCaptureSession, request: CaptureRequest): Observable<CaptureSessionData> {
-//        return Observable
-//            .create { observableEmitter -> captureSession.capture(request, createCaptureCallback(observableEmitter), null)}
-//    }
-//
-//    private fun setupSurface(surfaceTexture: SurfaceTexture) {
-//        surfaceTexture.setDefaultBufferSize(
-//            mCameraParams.previewSize.getWidth(),
-//            mCameraParams.previewSize.getHeight()
-//        )
-//        mSurface = Surface(surfaceTexture)
-//    }
-//
-//    fun createOnImageAvailableObservable(imageReader: ImageReader): Observable<ImageReader> {
-//        return Observable.create<ImageReader> { subscriber ->
-//
-//            val listener  = { reader: ImageReader ->
-//                if (!subscriber.isDisposed) {
-//                    subscriber.onNext(reader)
-//                }
-//            }
-//            imageReader.setOnImageAvailableListener(listener, null)
-//            subscriber.setCancellable {
-//                imageReader.setOnImageAvailableListener(null, null)
-//            }
-//        }
-//    }
-//
-//    fun save(image: Image, file: File): Single<File> {
-//        return Single.fromCallable {
-//            image.use { image ->
-//                var output: FileChannel = FileOutputStream(file).channel
-//                output.write(image.planes[0].buffer)
-//                file
-//            }
-//        }
-//    }
-//
-//    private fun initImageReader() {
-//    var sizeForImageReader: Size = CameraStrategy.getStillImageSize(mCameraParams.cameraCharacteristics, mCameraParams.previewSize);
-//    mImageReader = ImageReader.newInstance(sizeForImageReader.getWidth(), sizeForImageReader.getHeight(), ImageFormat.JPEG, 1);
-//    mCompositeDisposable.add(
-//        ImageSaverRxWrapper.createOnImageAvailableObservable(mImageReader)
-//            .observeOn(Schedulers.io())
-//            .flatMap(imageReader -> ImageSaverRxWrapper.save(imageReader.acquireLatestImage(), mFile).toObservable())
-//            .observeOn(AndroidSchedulers.mainThread())
-//            .subscribe(file -> mCallback.onPhotoTaken(file.getAbsolutePath(), getLensFacingPhotoType()))
-//    );
-//}
-
-
-//}
